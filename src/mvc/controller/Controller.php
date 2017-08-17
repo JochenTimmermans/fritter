22fa40ecc13d72c4d5fab117da2ae6e7470e6658
jfreeman <jfreeman@skedaddling.com>
2017-08-17 15:21:08 +0200

    installed controller and .htaccess

---
+++ src/mvc/controller/Controller.php
@@ -0,0 +1,107 @@
+<?php
+namespace freest\blog\mvc\controller;
+
+use freest\blog\mvc\model\Model as Model;
+use freest\blog\mvc\view\View as View;
+use freest\router\Router as Router;
+
+use freest\blog\mvc\controller\admin\AdminController as AdminController;
+
+// Twig modules
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
+/* 
+ * Controller.php
+ */
+
+class Controller 
+{
+    protected $model;
+    protected $view;
+    
+    protected $router;
+    
+    protected $twig;
+    protected $twigarr;
+    
+    public function __construct() {
+        $this->setTwig();
+    }
+    
+    private function setTwig() {
+        $loader = new Twig_Loader_Filesystem(ROOT_URL.'src/views/');
+        $this->twig = new Twig_Environment($loader, array(
+            'cache' => false
+        ));
+        $this->twigarr = array('site_title' => SITE_TITLE, 
+            'www' => BASE_URL,
+            'site_subtitle' => SITE_SUBTITLE,
+            'site_about' => SITE_ABOUT,
+            'sidebar_archives' => Model::retrieve_archives());    
+    }
+
+    protected function setModel(Model $model) 
+    {
+        $this->model = $model;
+    }
+    protected function setView(View $view) 
+    {
+        $this->view = $view;
+    }
+    protected function setRouter(Router $router) {
+        $this->router = $router;
+    }
+    
+    protected function startRouter() 
+    {        
+        $router = new Router();
+        $router->route('',          '0');
+        $router->route('index.php', '0');
+        $router->route('articles',  '1');
+        $router->route('article',  '1');
+        $router->route('fbadmin',   '2');
+        $this->router = $router;
+    }
+    
+    public function invoke() 
+    {
+        $this->startRouter();
+        
+        switch ($this->router->get()) {
+            case '0':
+                /*
+                 * Home
+                 * 
+                 * We get x last articles
+                 */
+                //echo 'home';
+                $fc = new FrontController();
+                $fc->setRouter($this->router);
+                $fc->invoke();
+                break;
+            case '1':
+                // Articles
+                //echo 'articles';
+                $ac = new ArticleController();
+                $ac->setRouter($this->router);
+                $ac->invoke();
+                break;
+            case '2':
+                //echo 'admin';
+                require '../admin.config.php';
+                $am_con = new AdminController();
+                $am_con->setRouter($this->router);
+                $am_con->invoke();
+                break;
+            default:
+                //echo $this->router->get();
+                //echo 'prrr';
+                // front
+                $fc = new FrontController();
+                $fc->setRouter($this->router);
+                $fc->invoke();
+        }    
+    }
+    
+}
