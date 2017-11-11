f98b5aa53444f2bd1008c4d2b6880d44fbd1cfe6
jfreeman <jfreeman@skedaddling.com>
2017-11-11 19:38:04 +0100

    altered namespace

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
+session_start();
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-use framework\mvc\controller\Controller as Controller;
+use fritter\mvc\controller\Controller as Controller;
 
 require '../global_config.php';
 
